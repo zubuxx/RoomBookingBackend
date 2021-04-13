58660ac36704d1d09e645c954b0417784a8c7779
zubuxx <krownicki1@gmail.com>
2021-04-13 23:47:08 +0200

    Initial commit

---
+++ src/main/java/pl/rownicki/roombooking/RoombookingApplication.java
@@ -0,0 +1,13 @@
+package pl.rownicki.roombooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoombookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoombookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/rownicki/roombooking/RoombookingApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.rownicki.roombooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoombookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
