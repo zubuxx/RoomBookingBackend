308f2fdd45b35fdcdcd1f0582c5ea53007a21cc2
zubuxx <krownicki1@gmail.com>
2021-04-16 12:34:41 +0200

    Bookings data initialization added

--- src/main/java/pl/rownicki/roombooking/service/DataInitialization.java
+++ src/main/java/pl/rownicki/roombooking/service/DataInitialization.java
@@ -4,12 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
+import pl.rownicki.roombooking.data.BookingRepository;
 import pl.rownicki.roombooking.data.RoomRepository;
 import pl.rownicki.roombooking.data.UserRepository;
-import pl.rownicki.roombooking.model.Layout;
-import pl.rownicki.roombooking.model.LayoutCapacity;
-import pl.rownicki.roombooking.model.Room;
-import pl.rownicki.roombooking.model.User;
+import pl.rownicki.roombooking.model.*;
 
 import java.util.List;
 
@@ -18,11 +16,13 @@ public class DataInitialization {
 
     private final RoomRepository roomRepository;
     private final UserRepository userRepository;
+    private final BookingRepository bookingRepository;
 
     @Autowired
-    public DataInitialization(RoomRepository roomRepository, UserRepository userRepository) {
+    public DataInitialization(RoomRepository roomRepository, UserRepository userRepository, BookingRepository bookingRepository) {
         this.roomRepository = roomRepository;
         this.userRepository = userRepository;
+        this.bookingRepository = bookingRepository;
     }
 
     @EventListener(ApplicationReadyEvent.class)
@@ -47,27 +47,27 @@ public class DataInitialization {
         User user = new User("matt", "secret");
         userRepository.save(user);
 
-//        Booking booking1 = new Booking();
-//        booking1.setDate(new java.sql.Date(new java.util.Date().getTime()));
-//        booking1.setStartTime(java.sql.Time.valueOf("11:00:00"));
-//        booking1.setEndTime(java.sql.Time.valueOf("11:30:00"));
-//        booking1.setLayout(Layout.USHAPE);
-//        booking1.setParticipants(8);
-//        booking1.setTitle("Conference call with CEO");
-//        booking1.setRoom(blueRoom);
-//        booking1.setUser(user);
-//        bookingRepository.save(booking1);
-//
-//        Booking booking2 = new Booking();
-//        booking2.setDate(new java.sql.Date(new java.util.Date().getTime()));
-//        booking2.setStartTime(java.sql.Time.valueOf("13:00:00"));
-//        booking2.setEndTime(java.sql.Time.valueOf("14:30:00"));
-//        booking2.setLayout(Layout.BOARD);
-//        booking2.setParticipants(5);
-//        booking2.setTitle("Sales Update");
-//        booking2.setRoom(redRoom);
-//        booking2.setUser(user);
-//        bookingRepository.save(booking2);
+        Booking booking1 = new Booking();
+        booking1.setDate(new java.sql.Date(new java.util.Date().getTime()));
+        booking1.setStartTime(java.sql.Time.valueOf("11:00:00"));
+        booking1.setEndTime(java.sql.Time.valueOf("11:30:00"));
+        booking1.setLayout(Layout.USHAPE);
+        booking1.setParticipants(8);
+        booking1.setTitle("Conference call with CEO");
+        booking1.setRoom(blueRoom);
+        booking1.setUser(user);
+        bookingRepository.save(booking1);
+
+        Booking booking2 = new Booking();
+        booking2.setDate(new java.sql.Date(new java.util.Date().getTime()));
+        booking2.setStartTime(java.sql.Time.valueOf("13:00:00"));
+        booking2.setEndTime(java.sql.Time.valueOf("14:30:00"));
+        booking2.setLayout(Layout.BOARD);
+        booking2.setParticipants(5);
+        booking2.setTitle("Sales Update");
+        booking2.setRoom(redRoom);
+        booking2.setUser(user);
+        bookingRepository.save(booking2);
     }
 }
 
